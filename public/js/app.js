864f0b6ad16f5d5eff0a758c10e89eacee93cb0d
gregorywaynepower <gregorywpower@gmail.com>
2019-09-23 23:11:46 -0400

    Added comments to explain code.

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 
-// pokemon
-// create data for 3 different pokemons, with their names, type, weaknesses, health, and attack moves(name, attack stat, maximum)
+// This is the simulated database, an array that holds all of our objects, with all their key-value pairs.
+
 var pokemonsDB = [
     {
       name: 'Charmander',
@@ -31,52 +31,91 @@ var pokemonsDB = [
     },  
   ]
 
+// Gamestate, states what pokemon is present.
+
 var gameState = {
   userPokemon: '',
   rivalPokemon: ''
 }
 
+// We pokemonsEl represents the elements with the classname "character", within the class "select-screen".
+
 var pokemonsEl = document.querySelector('.select-screen').querySelectorAll('.character');
+
+// This console.log gives us a chance to look at what elements we have selected.
+
 console.log(pokemonsEl);
+
+// Another way to grab an element, in this case we are selecting the element with the ID "battle-screen".
+
 var battleScreenEl = document.getElementById('battle-screen');
+
 var i = 0;
 
+// Our first loop.
+
 while (i < pokemonsEl.length ) {
+
+  // Adds a function that is activated when we click on any of the elements.
+
   pokemonsEl[i].onclick = function() {
+    // var pokemonName is assigned the pokemon's name from a value within the HTML attribute "data-pokemon".
     var pokemonName = this.dataset.pokemon;
+
     // console.log(pokemonName + ', I choose you!')
+
     var player1Img = document.querySelector('.player1').getElementsByTagName('img')
-    // Above variable lets us select the img inside the element with the class name "player1". Which will be useful later.
+
+    // Above variable lets us select the img inside the element with the class name "player1".
+
+    // Below variable player2Img lets us select the img inside the element with the class name "player2".
+
     var player2Img = document.querySelector('.player2').getElementsByTagName('img');
 
+    // We save the pokemon's name to the gamestate with this declaration.
+
     gameState.userPokemon = pokemonName;
+
+    // The computer assigns a random pokemon to the gameState object with "cpuPick();".
+
     cpuPick();
 
+    // Changes the screen to the battle screen by changing the position of the background.
+
     battleScreenEl.classList.toggle('active');
-    
+
+    // From the inside to to the outside: the string at "pokemon.name" is returned if it is equal to the string at "gameState.userPokemon". The returned string at "pokemon.name" via filter(), so a new array is created inside pokemonsDB. This value will be assigned as currentPokemon.
+
     var currentPokemon = pokemonsDB.filter(function(pokemon){
       return pokemon.name == gameState.userPokemon
     });
 
-    // From the inside to to the outside: the string at "pokemon.name" is returned if it is equal to the string at "gameState.userPokemon". The returned string at "pokemon.name" via filter(), so a new array is created inside pokemonsDB. This value will be assigned as currentPokemon.
+    // This assignment changes the value of the image.
+
+    player1Img[0].src = currentPokemon[0].img;
+
+    // The same process is carried out as it was with the currentPokemon variable.
 
     var currentRivalPokemon = pokemonsDB.filter(function(pokemon){
       return pokemon.name == gameState.rivalPokemon
     });
 
-    player1Img[0].src = currentPokemon[0].img;
-    player2Img[0].src = currentRivalPokemon[0].img;
+    // Changes values of image.
 
-    // Changes values of images.
+    player2Img[0].src = currentRivalPokemon[0].img;
 
     console.log(currentPokemon);
   }
   i++
 }
 
+// Function randomNumber was a solution that I Stackoverflow'ed to be able to generate a random number.
+
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+// Here we are putting parameters into the "randomNumber()" function where the rival could be picked from any of the first, second, or third attributes within the HTML elements that we have selected earlier, which exist as an array.
 
 function cpuPick() {
   gameState.rivalPokemon = pokemonsEl[randomNumber(0, 3)].dataset.pokemon
